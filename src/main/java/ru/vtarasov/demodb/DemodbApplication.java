8e164aecdbe2c2815d827128f7a2e6c83451bdcb
Vladimir Tarasov <vtarasov@naumen.ru>
2018-11-02 21:24:18 +0500

    Initial commit

---
+++ src/main/java/ru/vtarasov/demodb/DemodbApplication.java
@@ -0,0 +1,12 @@
+package ru.vtarasov.demodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodbApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/vtarasov/demodb/DemodbApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.vtarasov.demodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
